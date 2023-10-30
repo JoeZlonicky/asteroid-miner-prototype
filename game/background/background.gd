e23f984153409585ba4fddacea73f6c706c4a554
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-10-30 14:38:21 -0700

    Parallax

--- game/background/background.gd
+++ game/background/background.gd
@@ -1,13 +1,21 @@
-extends ParallaxBackground
+extends CanvasLayer
 
+
+const FAR_PARALLAX: float = 0.1
+const CLOSE_PARALLAX: float = 0.5
 
 @export var camera: Camera2D = null
 
-@onready var stars: ColorRect = $ParallaxLayer/Stars
+@onready var far_stars: ColorRect = $FarStars
+@onready var close_stars: ColorRect = $CloseStars
 
 
 func _process(_delta: float) -> void:
 	if camera == null:
 		return
 	
-	stars.material.set("shader_parameter/camera_offset", camera.global_position)
+	far_stars.material.set("shader_parameter/camera_offset", 
+		camera.global_position * FAR_PARALLAX)
+	
+	close_stars.material.set("shader_parameter/camera_offset", 
+		camera.global_position * CLOSE_PARALLAX)
