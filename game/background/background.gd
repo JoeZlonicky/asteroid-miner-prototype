766b8927cb4622f7a022c4012673b638ded21f4b
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-10-30 18:02:42 -0700

    Added pick-up

--- game/background/background.gd
+++ game/background/background.gd
@@ -2,11 +2,13 @@ extends CanvasLayer
 
 
 const FAR_PARALLAX: float = 0.05
-const CLOSE_PARALLAX: float = 0.3
+const MEDIUM_PARALLAX: float = 0.3
+const CLOSE_PARALLAX: float = 1.0
 
 @export var camera: Camera2D = null
 
 @onready var far_stars: ColorRect = $FarStars
+@onready var medium_stars: ColorRect = $MediumStars
 @onready var close_stars: ColorRect = $CloseStars
 
 
@@ -16,6 +18,9 @@ func _process(_delta: float) -> void:
 	
 	far_stars.material.set("shader_parameter/camera_offset", 
 		camera.global_position * FAR_PARALLAX)
+	
+	medium_stars.material.set("shader_parameter/camera_offset",
+		camera.global_position * MEDIUM_PARALLAX)
 	
 	close_stars.material.set("shader_parameter/camera_offset", 
 		camera.global_position * CLOSE_PARALLAX)
