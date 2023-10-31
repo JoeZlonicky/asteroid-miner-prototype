fd1383a4c5cbd1c902e3b5c5ab48654d09185227
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-10-31 14:56:57 -0700

    Tweaked ship visuals

--- player/equipment/thrusters/thrusters.gd
+++ player/equipment/thrusters/thrusters.gd
@@ -14,9 +14,12 @@ const MAX_ROTATION_THRUST: float = 2.0
 var current_rotation_thrust: float = 0.0
 
 @onready var trail: GPUParticles2D = $Trail
+@onready var trail2: GPUParticles2D = $Trail2
 
 
 func active_update(delta: float) -> void:
+	trail.emitting = false
+	trail2.emitting = false
 	process_velocity(delta)
 	process_rotation(delta)
 
@@ -33,12 +36,11 @@ func process_velocity(delta: float) -> void:
 		owner.velocity += forward_input * FORWARD_THRUST * delta * direction
 		owner.velocity = owner.velocity.limit_length(MAX_FORWARD_THRUST)
 		trail.emitting = true
+		trail2.emitting = true
 	elif forward_input < 0:
 		owner.velocity = owner.velocity.move_toward(Vector2.ZERO, SLOW_THRUST * delta)
-		trail.emitting = false
 	else:
 		owner.velocity = owner.velocity.move_toward(Vector2.ZERO, FORWARD_THRUST_DECELERATION * delta)
-		trail.emitting = false
 	
 	owner.move_and_slide()
 
@@ -47,8 +49,10 @@ func process_rotation(delta: float) -> void:
 	var rotation_input: int = 0
 	if Input.is_action_pressed("turn_left"):
 		rotation_input -= 1
+		trail2.emitting = true
 	if Input.is_action_pressed("turn_right"):
 		rotation_input += 1
+		trail.emitting = true
 	
 	if rotation_input == 0:
 		current_rotation_thrust = move_toward(current_rotation_thrust, 0.0, 
