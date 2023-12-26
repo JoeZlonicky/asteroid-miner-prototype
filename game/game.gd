6168bc16ab17c3846ca6948013ccfe3d4f88476e
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-25 20:20:22 -0800

    Reorganized project structure

--- game/game.gd
+++ game/game.gd
@@ -2,20 +2,7 @@ extends Node2D
 
 
 @onready var player: CharacterBody2D = $Player
-@onready var speed_label: Label = $HUD/SpeedLabel
-@onready var transmission_found_animator: AnimationPlayer = $HUD/TransmissionFoundText/AnimationPlayer
 
 
 func _ready() -> void:
 	DisplayServer.window_set_min_size(Vector2(800, 600))
-
-
-func _physics_process(_delta: float) -> void:
-	var player_speed: float = player.velocity.length()
-	player_speed = ceilf(player_speed - 0.01)  # Offset so it doesn't occasionally +1
-	player_speed = maxf(player_speed, 0.0)
-	speed_label.text = str(player_speed) + "km/s"
-
-
-func _on_transmission_picked_up():
-	transmission_found_animator.play("fade_in_and_out")
---
+++ ui/space_hud/space_hud.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+@export var player_ship: Player
+
+@onready var speed_label = $SpeedLabel
+
+
+func _physics_process(_delta: float) -> void:
+	var player_speed: float = player_ship.velocity.length()
+	player_speed = ceilf(player_speed - 0.01)  # Offset so it doesn't occasionally +1
+	player_speed = maxf(player_speed, 0.0)
+	speed_label.text = str(player_speed) + "km/s"
