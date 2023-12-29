39f816dea5f58c79b3a3490cce573ecf433100c7
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-29 15:13:27 -0800

    Interact with space station

---
+++ data/components/interact_area/interact_area.gd
@@ -0,0 +1,25 @@
+extends Component
+
+
+signal interacted
+
+var is_player_inside = false
+
+@onready var label = $Label
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and is_player_inside:
+		interacted.emit()
+
+
+func _on_trigger_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		is_player_inside = true
+		label.visible = true
+
+
+func _on_trigger_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		is_player_inside = false
+		label.visible = false
