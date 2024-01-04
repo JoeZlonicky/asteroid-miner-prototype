19618d861b5ad22f305f87f4ce6c873dcc8d6ddf
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-01-04 11:19:05 -0800

    Improve tread logic

--- data/components/treads/treads.gd
+++ data/components/treads/treads.gd
@@ -2,25 +2,36 @@ class_name Treads
 extends Component
 
 
-const MOVE_FORCE: float = 500.0
-const MAX_SPEED: float = 500.0
-const DECELERATION: float = 1500.0
+const DECELERATION: float = 2000.0
+const MAX_SPEED: float = 650.0
+const ROTATION_SPEED: float = 10.0
+
+var last_input_direction: Vector2 = Vector2.ZERO
 
 
 func active_update(delta: float) -> void:
-	process_velocity(delta)
-
-
-func process_velocity(delta: float) -> void:
 	var horizontal_input: float = Input.get_axis("move_left", "move_right")
 	var vertical_input: float = Input.get_axis("move_up", "move_down")
+	var input := Vector2(horizontal_input, vertical_input)
 	
-	if not horizontal_input and not vertical_input:
-		entity.velocity = entity.velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
-		entity.move_and_slide()
+	process_velocity(input, delta)
+	process_rotation(input, delta)
+
+
+func process_velocity(input_vector: Vector2, delta: float):
+	if input_vector:
+		entity.velocity = input_vector.normalized() * MAX_SPEED
+	else:
+		entity.velocity = entity.velocity.move_toward(Vector2.ZERO, DECELERATION)
+	entity.move_and_slide()
+
+
+func process_rotation(input_vector: Vector2, delta: float):
+	if input_vector:
+		last_input_direction = input_vector.normalized()
+	
+	if not last_input_direction:
 		return
 	
-	var input := Vector2(horizontal_input, vertical_input).normalized()
-	entity.velocity += input * MOVE_FORCE * delta
-	entity.velocity = entity.velocity.limit_length(MAX_SPEED)
-	entity.move_and_slide()
+	entity.rotation = rotate_toward(entity.rotation, last_input_direction.angle(),
+		ROTATION_SPEED * delta)
